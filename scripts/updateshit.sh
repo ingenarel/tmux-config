6510ac85758d72f41574081ecd3bd5ea70e9cae2
ingenarel <ingenarelitems@gmail.com>
2025-01-25 22:12:32 +0600

    (feat): the F5 keybind updater now has notifications

---
+++ scripts/updateshit.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/zsh
+
+update_stuff() {
+    eval $1 &&
+    {
+        notify-send -u critical "$2" || hyprctl notify 1 10000 "rgb(ff1ea3)" "fontsize:30 $2"
+        paplay $HOME/.config/tmux/sounds/notif.mp3
+    } ||
+    {
+        notify-send -u critical "$2 failed!" "Updating $3..."
+        paplay $HOME/.config/tmux/sounds/error.mp3
+    }
+}
+
+update_stuff "sudo emerge --sync"                                                                           "Synced repositories; updating system packages..."
+update_stuff "sudo emerge --ask --verbose --update --deep --newuse @world --keep-going"                     "Updated system packages; updating live ebuilds..."
+update_stuff "sudo smart-live-rebuild"                                                                      "Updated live ebuilds; updating yazi plugins..."
+update_stuff "ya pack --upgrade"                                                                            "Updated yazi plugins; updating zsh plugins..."
+update_stuff "source $HOME/.local/share/zinit/zinit.git/zinit.zsh && zinit update --all"                    "Updated zsh plugins; updating cargo packages..."
+update_stuff "cargo install-update -a"                                                                      "Updated cargo packages; updating tmux plugins..."
+update_stuff "$HOME/.tmux/plugins/tpm/bin/clean_plugins && $HOME/.tmux/plugins/tpm/bin/update_plugins all"  "Updated tmux plugins; updating neovim plugins..."
+update_stuff "nvim --headless '+Lazy! clean' '+Lazy! update' '+MasonUpdate' +qa"                            "Updated neovim plugins; updating neovim-mason's packages..."
+update_stuff "nvim -c Mason"                                                                                "Updated neovim-mason's packages"
